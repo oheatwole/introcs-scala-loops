0bec6afc6381b3be938eafc962a4b4d41eec8503
oheatwole <oheatwole@luc.edu>
2015-04-22 15:37:16 +0000

    first commit

--- loops.scala
+++ loops.scala
@@ -5,13 +5,16 @@ object loops {
   // Return a string containing n copies of s, end to end.
   // For example StringOfReps("Ok", 9) returns: "OkOkOkOkOkOkOkOkOk"
   def stringOfReps(s: String, n: Int): String = {
-    "unknown"
+    s*n
   }
 
   // Return n! (n factorial: 1*2*3 *...* n if n >=1; 0! is 1.).  
   //For example Factorial(4) returns 1*2*3*4 = 24.
-  def factorial(n: Int): Int = {
-    0
+  def factorial(n: BigInt): BigInt = {
+        if (n <= 1)
+           1  
+        else    
+           n * factorial(n - 1)
   }
 
 
@@ -23,8 +26,11 @@ object loops {
    //    eiiie
    //    eiiie
    //    eeeee
-   def printRectangle(columns:Int, rows:Int, inChar:String, edgeChar:String): String = {
-     "unknown"
+   def printRectangle(columns:Integer, rows:Integer, inChar:String, edgeChar:String): String = {
+       {edgeChar*(columns+2)
+        (edgeChar+(inChar*columns)+edgeChar)*rows
+        (edgeChar+(inChar*columns)+edgeChar)*rows
+        edgeChar*(columns+2)}
    }
 
 }
